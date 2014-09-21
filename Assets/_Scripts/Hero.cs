bafe7fa416959a5a1f366bd43c30a1d6a85a9bf7
jss367 <julius.simonelli@gmail.com>
2014-09-21 14:43:09 -0400

    From Asus

---
+++ Assets/_Scripts/Hero.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+
+	static public Hero S; //S for singleton
+
+	//These fields control the movement of the ship
+	public float speed = 30;
+	public float rollMult = -45;
+	public float pitchMult = 30;
+
+	//Ship status information
+	public float shiedLevel = 1;
+
+	public bool _____________;
+
+	void Awake(){
+				S = this; //Set the singleton
+		}
+
+	// Update is called once per frame
+	void Update () {
+	//Pull in information from the Input class
+		float xAxis = Input.GetAxis ("Horizontal");
+		float yAxis = Input.GetAxis ("Vertical");
+
+		//Change trainsform.position based on the axes
+		Vector3 pos = transform.position;
+		pos.x += xAxis * speed * Time.deltaTime;
+		pos.y += yAxis * speed * Time.deltaTime;
+		transform.position = pos;
+
+		//Rotate the ship to make it feel more dynamic
+		transform.rotation = Quaternion.Euler (yAxis * pitchMult, xAxis * rollMult, 0);
+	}
+}
