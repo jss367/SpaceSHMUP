bf5cd1ac02c9813e3f5cfbf0172051c85bb0cca7
jss367 <>
2014-09-21 23:19:31 -0400

    From  Lenovo
    These are changes from SUnday night

---
+++ Assets/_Scripts/Utils.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Utils : MonoBehaviour {
+
+
+	//=================== Bounds Functions ============
+
+	//Creates bounds that encapsulate the two Bounds passed in.
+	public static Bounds BoundsUnion (Bounds b0, Bounds b1){
+				//If the size of one of the bounds is Vector3.zero, ignore that one
+				if (b0.size == Vector3.zero && b1.size != Vector3.zero) {
+						return (b1);
+				}
+		//Why wouldn't you combine these two into "If b1.size == Vector3.zero return b0"?
+		else if (b0.size != Vector3.zero && b1.size == Vector3.zero) {
+						return (b0);
+				} else if (b0.size == Vector3.zero && b1.size == Vector3.zero) {
+						return(b0);
+				}
+				//Stretch b0 to include the b1.min and b1.max
+				b0.Encapsulate (b1.min);
+				b0.Encapsulate (b1.max);
+				return(b0);
+		}
+
+	public static
+
+	}
